2bdab8747f4813a2b018262bd904642399fb9f2a
mtrangcp <haminhtrang545@gmail.com>
2025-04-01 09:15:09 +0700

    mmm

--- bt6/ss36_bt6.js
+++ bt6/ss36_bt6.js
@@ -1,17 +1,63 @@
-
+let selectedRating = document.querySelector("#selected-rating");
 const stars = document.querySelectorAll(".stars");
+const formEl = document.querySelector("#formEl");
+const cmtEl = document.querySelector("#comment-input");
+
+let starInput = 0;
+let cmtLocals = JSON.parse(localStorage.getItem("cmts")) || [];
+let listCmt = document.querySelector("#comment-list");
+const renderData = () => {
+    listCmt.innerHTML = "";
+
+    listCmt.innerHTML = cmtLocals.map(item => {
+        return `
+            <li>
+                <div>${"★".repeat(item.star)}</div>
+                <p>${item.content}</p>
+            </li>
+        `;
+    }).join("");
+}
+renderData();
 
 stars.forEach((star, index) => {
     star.addEventListener("mouseover", function (e) {
-        star.classList.toggle("active");
+        star.classList.add("active");
 
+        // console.log(e.target.getAttribute("data"));
 
+        starValue = e.target.getAttribute("data");
 
-        console.log(document.querySelector(`.stars[data-value="${index + 1}"]`));
+        for (let i = 0; i < starValue; i++) {
+            stars[i].classList.add("active");
+        }
 
+        for (let i = starValue; i < stars.length; i++) {
+            stars[i].classList.remove("active");
+        }
 
-
+        starInput = starValue;
+        selectedRating.textContent = `Bạn đã đánh giá ${starInput} sao`;
     });
 });
 
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    let newObj = {
+        id: Math.ceil(Math.random() * 100000),
+        content: cmtEl.value.trim(),
+        star: +starInput
+    }
+
+    cmtLocals.push(newObj);
+    localStorage.setItem("cmts", JSON.stringify(cmtLocals));
+
+    cmtEl.value = "";
+
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove("active");
+    }
+
+    renderData();
+});
